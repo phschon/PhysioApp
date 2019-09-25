7f57cb9ac03e16ac48a8a841bd1aabdbeb128b6e
phschon <p-schon@freenet.de>
2019-09-25 17:44:30 +0200

    Initial dummy content

--- CF_ApplicationBackEnd/src/main/java/physio/api/DummyContent.java
+++ CF_ApplicationBackEnd/src/main/java/physio/api/DummyContent.java
@@ -1,10 +1,21 @@
 package physio.api;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -29,20 +40,33 @@ public class DummyContent {
 	Therapist2PatientRepository therapist2Patient;
 
 	private String admin = "   {\n" + "        \"id\": \"a1\",\n" + "        \"email\": \"admin@123.de\",\n" + "        \"role\": \"a\",\n" + "        \"firstName\": \"test\",\n" + "        \"lastName\": \"admin\",\n" + "        \"address\": \"address\",\n" + "        \"imageUrl\": \"www.de\"\n" + "    }";
-	private String patient = "   {\n" + "        \"id\": \"p1\",\n" + "        \"email\": \"patient@123.de\",\n" + "        \"role\": \"p\",\n" + "        \"firstName\": \"test\",\n" + "        \"lastName\": \"patient\",\n" + "        \"address\": \"address\",\n" + "        \"imageUrl\": \"www.de\"\n" + "    }";
-	private String therapist = "   {\n" + "        \"id\": \"t1\",\n" + "        \"email\": \"therapist@123.de\",\n" + "        \"role\": \"t\",\n" + "        \"firstName\": \"test\",\n" + "        \"lastName\": \"therapist\",\n" + "        \"address\": \"address\",\n" + "        \"imageUrl\": \"www.de\"\n" + "    }";
 
 	private String t2p = "{\"therapist\": \"t1\", \"patient\": \"p1\"}";
 
 	@EventListener(ApplicationReadyEvent.class)
 	public ResponseEntity fill() throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
+
+		// generate Patient data
+		File resource = new ClassPathResource("sampledata_patient.json").getFile();
+		String patients = new String(Files.readAllBytes(resource.toPath()));
+		JSONArray json = new JSONArray(patients);
+		for (Object s: json) {
+			userRepository.save(mapper.readValue(s.toString(), User.class));
+		}
+
+		// generate Patient data
+		resource = new ClassPathResource("sampledata_therapist.json").getFile();
+		String therapists = new String(Files.readAllBytes(resource.toPath()));
+		json = new JSONArray(therapists);
+		for (Object s: json) {
+			userRepository.save(mapper.readValue(s.toString(), User.class));
+		}
+
+		// dummy admin user
 		User a = mapper.readValue(admin, User.class);
 		userRepository.save(a);
-		User p = mapper.readValue(patient, User.class);
-		userRepository.save(p);
-		User t = mapper.readValue(therapist, User.class);
-		userRepository.save(t);
+
 		Therapist2Patient th2pa = mapper.readValue(t2p, Therapist2Patient.class);
 		//therapist2Patient.save(th2pa);
 
