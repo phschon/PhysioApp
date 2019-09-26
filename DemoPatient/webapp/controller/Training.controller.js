72b1242c2ffc6c2e6e3f7ebfd2b5bd530d0e46fa
Peter Hoffmann <ph@strongpeople.institute>
2019-09-26 08:15:18 +0000

    Initial commit

---
+++ DemoPatient/webapp/controller/Training.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("DemoPatient.DemoPatient.controller.Training", {
+		onInit: function () {
+		},
+		
+		onDisplayProgress: function(Event ) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("progress");
+		},		
+		
+		onDisplayMedicalFile: function(Event){
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("medicalFile");			
+		}
+	});
+});
