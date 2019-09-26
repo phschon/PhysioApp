a10f50da0bbf15022976b664edca62190de70647
d064289 <mario.schlereth@sap.com>
2019-09-26 17:53:53 +0200

    Complete getProgram request

--- CF_ApplicationBackEnd/src/main/java/physio/database/entities/Module.java
+++ CF_ApplicationBackEnd/src/main/java/physio/database/entities/Module.java
@@ -2,10 +2,14 @@ package physio.database.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,4 +28,8 @@ public class Module {
 
 	@NotNull
 	private String description;
+
+	@Transient
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private List<Exercise> exercise;
 }
